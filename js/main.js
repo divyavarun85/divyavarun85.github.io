8fb306010b5e67d36f18c50164c18c4c65ad7b3b
divyavarun85 <divyavarun85@gmail.com>
2021-03-19 14:21:48 -0400

    edited

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+/*Setting up the humburger Menu button */
+const menuBTn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav')
+const navItems = document.querySelectorAll('.menu-nav__item');
+const menuHTMLLoad = document.getElementsByClassName('.menupage');
+
+
+
+let showMenu = false;
+
+menuBTn.addEventListener('click',toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu){
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(item =>item.classList.add('open'));
+        showMenu = true;
+    }else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(item =>item.classList.remove('open'));
+        showMenu = false;
+    }
+}
+
